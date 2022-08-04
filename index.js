1d4b54545df187a2b6bc4aba03c1d496c74b239c
Mossahid123 <mossahidhossain309@gmail.comm>
2022-08-04 09:48:42 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 const twilio = require("twilio");
 const { disconnect } = require("process");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 app.use(cors());
+app.use(express.json());
 require('dotenv').config();
+
 
 // =============================
 //          Socket IO         //
@@ -25,7 +28,7 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-   // ======Create New Room=====
+  // ======Create New Room=====
   socket.on("new-room", (data) => {
     newRoomHandler(data, socket);
   });
@@ -46,13 +49,13 @@ io.on("connection", (socket) => {
     console.log("User Disconnected", socket.id);
   });
 
-   // ======Connection Signal=====
-   socket.on("connect-signal", (data) => {
+  // ======Connection Signal=====
+  socket.on("connect-signal", (data) => {
     signalingHandler(data, socket);
   });
 
-   // ======Connection Initialize=====
-   socket.on("connect-init", (data) => {
+  // ======Connection Initialize=====
+  socket.on("connect-init", (data) => {
     initializeConnectionHandler(data, socket);
   });
 
@@ -63,10 +66,45 @@ io.on("connection", (socket) => {
 //          MongoDB           //
 //==============================
 
+
+
+// mongodb
+
+
+
+const uri = `mongodb+srv://MeetRoom:sBSrQ544m8rYkRmd@cluster0.cgs9b.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ 
+async function run(){
+  try{
+    await client.connect();
+        const SlotCollection = client.db('MeetRoom').collection('meeting-slots');
+
+        app.get('/meeting-slots', async(req, res) =>{
+          const query = {};
+          const cursor = SlotCollection.find(query);
+          const slots = await cursor.toArray();
+          res.send(slots);
+      })
+  }
+  finally{}
+}
+run().catch(console.dir);
+
+// const uri = "mongodb+srv://MeetRoom:sBSrQ544m8rYkRmd@cluster0.cgs9b.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("MeetRoom").collection("meeting-slots");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 // Connceted with client server
 app.get('/', (req, res) => {
   res.send("MeetRoom server is running")
 })
+
+
 
 server.listen(PORT, () => {
   console.log(`MeetRoom server is running on ${PORT}`);

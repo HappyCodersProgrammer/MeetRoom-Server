77abdbac5d51505cd45f3393978049a465a7bcdb
hasan-mia <hasanrafi69@gmail.com>
2022-08-05 15:35:55 +0600

    added mongo

--- index.js
+++ index.js
@@ -27,6 +27,28 @@ const io = new Server(server, {
   },
 });
 
+// =======Inisitialize=======
+let connectedUsers = [];
+let rooms = [];
+
+// ========create route to check if room exists=======
+app.get("/api/room-exists/:roomId", (req, res) => {
+  const { roomId } = req.params;
+  const room = rooms.find((room) => room.id === roomId);
+
+  if (room) {
+    // send reponse that room exists
+    if (room.connectedUsers.length > 3) {
+      return res.send({ roomExists: true, full: true });
+    } else {
+      return res.send({ roomExists: true, full: false });
+    }
+  } else {
+    // send response that room does not exists
+    return res.send({ roomExists: false });
+  }
+});
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
@@ -63,25 +85,139 @@ io.on("connection", (socket) => {
 
 });
 
+//===========socket.io handlers===========
+
+const createNewRoomHandler = (data, socket) => {
+  console.log("host is creating new room");
+  console.log(data);
+  const { identity, onlyAudio } = data;
+
+  const roomId = uuidv4();
+
+  // create new user
+  const newUser = {
+    identity,
+    id: uuidv4(),
+    socketId: socket.id,
+    roomId,
+    onlyAudio,
+  };
+
+  // push that user to connectedUsers
+  connectedUsers = [...connectedUsers, newUser];
+
+  //create new room
+  const newRoom = {
+    id: roomId,
+    connectedUsers: [newUser],
+  };
+  // join socket.io room
+  socket.join(roomId);
+
+  rooms = [...rooms, newRoom];
+
+  // emit to that client which created that room roomId
+  socket.emit("room-id", { roomId });
+
+  // emit an event to all users connected
+  // to that room about new users which are right in this room
+  socket.emit("room-update", { connectedUsers: newRoom.connectedUsers });
+};
+
+const joinRoomHandler = (data, socket) => {
+  const { identity, roomId, onlyAudio } = data;
+
+  const newUser = {
+    identity,
+    id: uuidv4(),
+    socketId: socket.id,
+    roomId,
+    onlyAudio,
+  };
+
+  // join room as user which just is trying to join room passing room id
+  const room = rooms.find((room) => room.id === roomId);
+  room.connectedUsers = [...room.connectedUsers, newUser];
+
+  // join socket.io room
+  socket.join(roomId);
+
+  // add new user to connected users array
+  connectedUsers = [...connectedUsers, newUser];
+
+  // emit to all users which are already in this room to prepare peer connection
+  room.connectedUsers.forEach((user) => {
+    if (user.socketId !== socket.id) {
+      const data = {
+        connUserSocketId: socket.id,
+      };
+
+      io.to(user.socketId).emit("conn-prepare", data);
+    }
+  });
+
+  io.to(roomId).emit("room-update", { connectedUsers: room.connectedUsers });
+};
+
+const disconnectHandler = (socket) => {
+  // find if user has been registered - if yes remove him from room and connected users array
+  const user = connectedUsers.find((user) => user.socketId === socket.id);
+
+  if (user) {
+    // remove user from room in server
+    const room = rooms.find((room) => room.id === user.roomId);
+
+    room.connectedUsers = room.connectedUsers.filter(
+      (user) => user.socketId !== socket.id
+    );
+
+    // leave socket io room
+    socket.leave(user.roomId);
+
+    // close the room if amount of the users which will stay in room will be 0
+    if (room.connectedUsers.length > 0) {
+      // emit to all users which are still in the room that user disconnected
+      io.to(room.id).emit("user-disconnected", { socketId: socket.id });
+
+      // emit an event to rest of the users which left in the toom new connectedUsers in room
+      io.to(room.id).emit("room-update", {
+        connectedUsers: room.connectedUsers,
+      });
+    } else {
+      rooms = rooms.filter((r) => r.id !== room.id);
+    }
+  }
+};
+
+const signalingHandler = (data, socket) => {
+  const { connUserSocketId, signal } = data;
+
+  const signalingData = { signal, connUserSocketId: socket.id };
+  io.to(connUserSocketId).emit("connect-signal", signalingData);
+};
+
+// information from clients which are already in room that They have preapred for incoming connection
+const initializeConnectionHandler = (data, socket) => {
+  const { connUserSocketId } = data;
+
+  const initData = { connUserSocketId: socket.id };
+  io.to(connUserSocketId).emit("connect-init", initData);
+};
+
+
 
 // =============================
 //          MongoDB           //
 //==============================
 
-
-
-// mongodb
-
-
-
-
 const uri = "mongodb+srv://meetroom:meetroom12345@cluster0.cgs9b.mongodb.net/?retryWrites=true&w=majority";
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run() {
+// ======Main Function ========
+async function run () {
   try {
-    await client.connect();
+    client.connect();
+    console.log('Connected Successfuly');
     const scheduleCollection = client.db('MeetRoom').collection('meeting-slots');
 
     app.post('/schedule', async (req, res) => {
@@ -98,53 +234,16 @@ async function run() {
       const products = await cursor.toArray();
       res.send(products)
     });
-
-
-
   }
-  finally {
-
-  }
+  finally{}
 }
+run().catch(console.dir);
 
-run().catch(console.dir)
-
-
-
-
-// const uri = `mongodb+srv://MeetRoom:sBSrQ544m8rYkRmd@cluster0.cgs9b.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-// async function run(){
-//   try{
-//     await client.connect();
-//         const SlotCollection = client.db('MeetRoom').collection('meeting-slots');
-
-//         app.get('/meeting-slots', async(req, res) =>{
-//           const query = {};
-//           const cursor = SlotCollection.find(query);
-//           const slots = await cursor.toArray();
-//           res.send(slots);
-//       })
-//   }
-//   finally{}
-// }
-// run().catch(console.dir);
-
-// const uri = "mongodb+srv://MeetRoom:sBSrQ544m8rYkRmd@cluster0.cgs9b.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("MeetRoom").collection("meeting-slots");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
 // Connceted with client server
 app.get('/', (req, res) => {
   res.send("MeetRoom server is running")
 })
-
-
 
 server.listen(PORT, () => {
   console.log(`MeetRoom server is running on ${PORT}`);
